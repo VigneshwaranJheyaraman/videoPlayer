d9b55b4ad411f3ba32232b8829f2dbb77febeb1b
VigneshwaranJheyaraman <vigneshwaranjheyaraman@gmail.com>
2020-06-28 13:20:06 +0530

    1.subtitle updating on sync function, but need to handle on blur the setInterval freezes so to pause the video on blur 2.v2.3.1 patch update

--- src/js/player/SubtitleSynchronizer.js
+++ src/js/player/SubtitleSynchronizer.js
@@ -2,9 +2,10 @@ class SubtitleSynchronizer {
     constructor(subtitleDictionary) {
         this.__dictionary = subtitleDictionary;
         this.__currentSubtitle = null;
-        this.__currentTime = 0;
-        this.__milliSecOffset = 1;
-        this.__milliSecInterval = 1000;
+        this.__currentSec = 0;
+        this.__milliSecCount = 0;
+        this.__milliSecOffset = 100;
+        this.__milliSecInterval = 100;
         this.__synchronizing = false;
         this.__synchronizingIntervalHandler = null;
         this.__stopped = false;
@@ -15,7 +16,13 @@ class SubtitleSynchronizer {
         this.__clearSync = this.__clearSync.bind(this);
         this.__updateCurrentSubtitle = this.__updateCurrentSubtitle.bind(this);
         this.__updateTime = this.__updateTime.bind(this);
+        this.__runTimer = this.__runTimer.bind(this);
+        this.__synchronize = this.__synchronize.bind(this);
         this.__initializeSynchronizer = this.__initializeSynchronizer.bind(this);
+    }
+
+    get currentTime() {
+        return this.__currentSec + (this.__milliSecCount / 1000);
     }
 
     initSync() {
@@ -27,17 +34,25 @@ class SubtitleSynchronizer {
             this.initSync();
         }
         this.__synchronizing = true;
-        this.__updateCurrentSubtitle();
-        console.log(this.__currentSubtitle);
     }
 
     stopSync() {
         this.__stopped = true;
     }
 
+    __runTimer() {
+        if (this.__milliSecCount < 1000) {
+            this.__milliSecCount += this.__milliSecOffset;
+        } else {
+            this.__milliSecCount = 0;
+            this.__currentSec += 1;
+        }
+    }
+
     __clearSync() {
         clearInterval(this.__synchronizingIntervalHandler);
-        this.__currentTime = 0;
+        this.__currentSec = 0;
+        this.__milliSecCount = 0;
         this.__currentSubtitle = null;
         this.__synchronizing = false;
         this.__synchronizingIntervalHandler = null;
@@ -45,10 +60,11 @@ class SubtitleSynchronizer {
 
 
     __updateCurrentSubtitle() {
+        var currentTime = this.currentTime;
         Object.keys(this.__dictionary).forEach(key => {
             key = parseFloat(key);
-            if ((this.__currentTime - key) < 1 && (this.__currentTime - key) >= 0) {
-                if (this.__currentTime < this.__dictionary[key].to) {
+            if ((currentTime - key) < 1 && (currentTime - key) >= 0) {
+                if (currentTime < this.__dictionary[key].to) {
                     this.__currentSubtitle = this.__dictionary[key].subtitle;
                 }
                 return;
@@ -56,9 +72,16 @@ class SubtitleSynchronizer {
         });
     }
 
+    __synchronize() {
+        this.__updateTime();
+        this.__updateCurrentSubtitle();
+    }
+
+
     __updateTime() {
         if (this.__synchronizing) {
-            this.__currentTime += this.__milliSecOffset;
+            this.__runTimer();
+            console.log(this.__currentSubtitle, this.currentTime);
         }
         if (this.__stopped) {
             this.__clearSync();
@@ -67,6 +90,6 @@ class SubtitleSynchronizer {
 
     __initializeSynchronizer() {
         this.__stopped = false;
-        return setInterval(this.__updateTime, this.__milliSecInterval);
+        return setInterval(this.__synchronize, this.__milliSecInterval);
     }
 }
